cef9779287575de5543c70e1d3ea054f7f5cfb5e
Ítalo de Pontes Oliveira <italooliveira@copin.ufcg.edu.br>
2024-07-17 23:56:02 -0300

    Create content_search.py

---
+++ src/content_search.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+
+input_file_name = '../data/TCE_PB/cnpj_ug_dados_gov.csv'
+
+df = pd.read_csv(input_file_name)
+
+st.title("TCE PB - Search Tool")
+
+text = st.text_input("Digite o que você busca", "Unidade Gestora (UG), CNPJ, ou Descrição.")
+
+def search(text: str = '',
+           variables: list = ()) -> bool:
+    text = str(text).lower()
+    return any(text in str(field).lower() for field in variables)
+
+df = df[df.apply(lambda x: search(text,
+                                  [x['CNPJ'],
+                                  x['cd_Ugestora'],
+                                  x['NOME FANTASIA'],
+                                  x['Cidade']]), axis=1)]
+
+st.dataframe(df)
